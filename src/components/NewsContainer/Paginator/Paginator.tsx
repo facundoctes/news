76a28b43f65bafbf85e376676dd6dd72a0a8ffd4
Facundo Herman Gomez Gottschalk <facundo1988@hotmail.com>
2021-05-01 21:03:31 -0300

    [fix] paginator

--- src/components/NewsContainer/Paginator/Paginator.tsx
+++ src/components/NewsContainer/Paginator/Paginator.tsx
@@ -13,11 +13,11 @@ const Paginator = React.memo((props: IPaginatorProps) => {
         const items = [];
 
         const totalPages:number = Math.ceil(totalCount/pageSize);
-        const start: number = (page-5) < 1 ? 1 : (page-5);
-        const end: number = (start + 10) > totalPages ? totalPages : (start + 10);
+        const start: number = (page-3) < 1 ? 1 : (page-3);
+        const end: number = (start + 6) > totalPages ? totalPages : (start + 6);
 
         if (start > 1) {
-            items.push(<Pagination.First key="first" onClick={() => props.setPage(0)} />);
+            items.push(<Pagination.First key="first" onClick={() => props.setPage(1)} />);
         }
 
         for (let x=start; x <= end; x++ ) {
